8b95848443de5fd5405c201fa04cb4f35434f430
Karlatemp <karlatemp@vip.qq.com>
2021-12-08 22:47:13 +0800

    Secrets Protection

---
+++ mirai-core-api/src/jvmTest/kotlin/utils/SecretsProtectionTest.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2019-2021 Mamoe Technologies and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
+ *
+ * https://github.com/mamoe/mirai/blob/dev/LICENSE
+ */
+
+package net.mamoe.mirai.utils
+
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Test
+import kotlin.test.assertTrue
+
+internal class SecretsProtectionTest {
+    @Test
+    fun chaosTest() = runBlocking<Unit> {
+        repeat(500) {
+            launch {
+                val data = ByteArray((1..255).random()) { (0..255).random().toByte() }
+                val buffer = SecretsProtection.escape(data).readBytes()
+                assertTrue { data.contentEquals(buffer) }
+            }
+        }
+    }
+}
